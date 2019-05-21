a725b3a8f64a299bbbe3d56b078ec6b8b62529a8
glii0012 <glii0012@student.monash.edu>
2019-05-21 15:23:03 +1000

    CustomerController
    add one field and two methods

--- src/com/me/CustomerController.java
+++ src/com/me/CustomerController.java
@@ -1,5 +1,28 @@
 package com.me;
 
-public class CustomerController {
+import java.util.ArrayList;
 
+public class CustomerController {
+	// instances of customers
+	private Customer customer;
+	/*
+     * Constructor for objects of class CustomerController
+     */
+	public CustomerController() {
+        customer = new Customer();
+    }
+	
+	/*
+	 * Method to view Cart
+	 */
+	public ArrayList<Item> viewCart() {
+		
+	}
+	
+	/*
+	 * Method to edit Cart
+	 */
+	public void editCart() {
+		
+	}
 }
