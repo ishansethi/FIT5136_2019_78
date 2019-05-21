e18a69b4917a3d30c99299fb1c9e487fa930022f
glii0012 <glii0012@student.monash.edu>
2019-05-21 16:28:12 +1000

    admin + admincontroller + owner

--- src/com/me/AdminController.java
+++ src/com/me/AdminController.java
@@ -1,5 +1,20 @@
 package com.me;
 
 public class AdminController {
+	
+	/*
+	 * Method for registering an owner
+	 */
+	public void registerOwner(String firstName, String lastName, String email, String password) {
+		
+		Owner newOwner = new Owner(firstName, lastName, email, password);
+	}
+	
+	/*
+	 * Method for deleting an owner
+	 */
+	public void deleteOwner(Owner owner) {
+		
+	}
 
 }
---
+++ src/com/me/Owner.java
@@ -0,0 +1,67 @@
+package com.me;
+
+public class Owner {
+	// instance variables
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String phoneNo;
+	private String email;
+	private String password;
+
+	// creating instance of customer with mandatory fields
+	public Owner(String firstName, String lastName, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
