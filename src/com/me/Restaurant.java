44923e60128ae59dfc89a08993cf9d398bcb6f10
ishan <iset0002@student.monash.edu>
2019-05-21 16:15:54 +1000

    err1

--- src/com/me/Restaurant.java
+++ src/com/me/Restaurant.java
@@ -1,5 +1,19 @@
 package com.me;
 
 public class Restaurant {
+	
+	
+	
+	public Restaurant() 
+	{
+		
+	}
 
+	public Restaurant(String resId, String resName, String resAdd, String resSub, int resPcode,  String resEmail, String resPhone, String resCuizine) {
+		
+		
+		
+	}
+	
+	
 }
