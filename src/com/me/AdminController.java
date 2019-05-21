4ce17e6c80eafbdd6de6a28ed346249bc32f71db
glii0012 <glii0012@student.monash.edu>
2019-05-21 23:25:47 +1000

    AdminController updated

--- src/com/me/AdminController.java
+++ src/com/me/AdminController.java
@@ -1,9 +1,12 @@
 package com.me;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AdminController {
+	
 	private static ArrayList<Owner> ownerList = new ArrayList<Owner>();
+	Scanner scanner = new Scanner(System.in);
 	
 	public static ArrayList<Owner> getOwnerList() {
 		return ownerList;
@@ -12,22 +15,34 @@ public class AdminController {
 	/*
 	 * Method for registering an owner
 	 */
-	public void registerOwner(String firstName, String lastName, String email, String password) {
-		Owner newOwner = new Owner(firstName, lastName, email, password);
+	public void registerOwner() {//String firstName, String lastName, String email, String password) {
+		//Owner newOwner = new Owner(firstName, lastName, email, password);
+		Owner newOwner = new Owner("", "", "", "");
+		System.out.println("Please enter a first name: ");
+		newOwner.setFirstName(scanner.nextLine());
+		System.out.println("Please enter a last name: ");
+		newOwner.setLastName(scanner.nextLine());
+		System.out.println("Please enter email address: ");
+		newOwner.setEmail(scanner.nextLine());
+		System.out.println("Please enter password: ");
+		newOwner.setPassword(scanner.nextLine());
 		ownerList.add(newOwner);
 	}
 	
 	/*
 	 * Method for deleting an owner
 	 */
-	public void deleteOwner(String email) {
+	public void deleteOwner() {
+		System.out.println("Please enter an email address: ");
+		String email = scanner.nextLine();
 		for (Owner owner : ownerList) {
 			if (owner.getEmail().equals(email)) {
 				ownerList.remove(owner);
+				System.out.print("This owner has been deleted");
 				break;
 			}
 			else {
-				System.out.print("Owner not found");
+				System.out.print("Owner does not exist");
 			}
 		}
 	}
