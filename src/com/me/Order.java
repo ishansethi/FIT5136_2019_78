130ee14e499872e607ad3c6fad5aba1e95f77ad0
glii0012 <glii0012@student.monash.edu>
2019-05-21 22:49:34 +1000

    order

--- src/com/me/Order.java
+++ src/com/me/Order.java
@@ -1,5 +1,58 @@
 package com.me;
 
+import java.util.Date;
+
 public class Order {
+	// Fields of an Order object
+	private String id;
+	private String custId;
+	private String status;
+	private Date dateCreated;
+	private float totalCost;
+	
+	// Constructor of class Order
+	public Order() {
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public float getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(float totalCost) {
+		this.totalCost = totalCost;
+	}
 
 }
